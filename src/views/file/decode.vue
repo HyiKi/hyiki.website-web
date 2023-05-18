560f3b609e3c044252269e4cbd407d1f8bf90051
HyiKi <240456539@qq.com>
2023-05-19 03:32:15 +0800

    download file

---
+++ src/views/file/decode.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <el-divider direction="vertical" class="vertical-divider" />
+    <span class="text">输入信息</span>
+    <el-form ref="inputForm" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="download file">
+        <el-input v-model="input" type="textarea" :rows="4" placeholder="请输入需要提取文件的code" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">下载</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import clipboard from '@/directive/clipboard/index.js' // use clipboard by v-directive
+import axios from 'axios'
+
+export default {
+  directives: {
+    clipboard
+  },
+  data() {
+    return {
+      labelPosition: 'top',
+      input: '',
+      output: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      const code = this.input
+      axios({
+        url: `https://www.hyiki.website:8099/file/decode?input=${code}`,
+        method: 'get',
+        responseType: 'blob'
+      })
+        .then(res => {
+          const contentDisposition = decodeURIComponent(res.headers['content-disposition'])
+          const fileName = contentDisposition.split('filename=')[1].replace(/"/g, '')
+          const url = window.URL.createObjectURL(new Blob([res.data]))
+          const link = document.createElement('a')
+          link.href = url
+          link.setAttribute('download', fileName)
+          document.body.appendChild(link)
+          link.click()
+        })
+      this.$message({
+        message: '下载正在执行',
+        type: 'success',
+        duration: 1500
+      })
+    },
+    clipboardSuccess() {
+      this.$message({
+        message: '复制成功',
+        type: 'success',
+        duration: 1500
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 1rem;
+}
+
+.vertical-divider {
+  border-left: #2468f2 0.25rem solid;
+  margin: 12px 0px;
+  padding: 0 0 0 14px;
+  background: transparent;
+  line-height: 1.2;
+}
+
+.text {
+  font-size: 14px;
+}
+</style>
