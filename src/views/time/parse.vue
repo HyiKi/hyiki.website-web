44d2012bde503dc30409e35e8972248e0411f9aa
HyiKi <240456539@qq.com>
2023-06-14 09:28:44 +0800

    Update parse.vue

--- src/views/time/parse.vue
+++ src/views/time/parse.vue
@@ -3,8 +3,23 @@
     <el-divider direction="vertical" class="vertical-divider" />
     <span class="text">输入信息</span>
     <el-form ref="inputForm" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
-      <el-form-item label="string">
-        <el-input v-model="input" type="textarea" :rows="4" placeholder="请输入需要转义的字符串" />
+      <el-form-item label="type">
+        <el-radio v-model="radio" label="current">当前</el-radio>
+        <el-radio v-model="radio" label="date">日期</el-radio>
+        <el-radio v-model="radio" label="timestamp">时间戳</el-radio>
+        <el-radio v-model="radio" label="datetime">时间</el-radio>
+      </el-form-item>
+      <el-form-item label="日期">
+        <el-input id="date" v-model="date" />
+      </el-form-item>
+      <el-form-item label="时间戳">
+        <el-input id="timestamp" v-model="timestamp" />
+      </el-form-item>
+      <el-form-item label="时间类型">
+        <el-input id="dateType" v-model="dateType" placeholder="请选择时间类型" />
+      </el-form-item>
+      <el-form-item label="时间">
+        <el-input id="datetime" v-model="datetime" placeholder="时间" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">提交</el-button>
@@ -14,13 +29,26 @@
     <el-divider direction="vertical" class="vertical-divider" />
     <span class="text">输出信息</span>
     <el-form ref="outputForm" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
-      <el-form-item label="escaped string">
-        <el-input id="output" v-model="output" disabled type="textarea" :rows="4" placeholder="经过转义的字符串" />
+      <el-form-item label="日期">
+        <el-input id="dateVal" v-model="dateVal" disabled placeholder="yyyy/MM/dd HH:mm:ss" />
       </el-form-item>
-      <el-form-item>
-        <el-button v-clipboard:copy="output" v-clipboard:success="clipboardSuccess" type="primary" icon="el-icon-document">
-          复制
-        </el-button>
+      <el-form-item label="时间戳">
+        <el-input id="timestampVal" v-model="timestampVal" disabled placeholder="" />
+      </el-form-item>
+      <el-form-item label="天">
+        <el-input id="dayVal" v-model="dayVal" disabled />
+      </el-form-item>
+      <el-form-item label="时">
+        <el-input id="hourVal" v-model="hourVal" disabled />
+      </el-form-item>
+      <el-form-item label="分">
+        <el-input id="minuteVal" v-model="minuteVal" disabled />
+      </el-form-item>
+      <el-form-item label="秒">
+        <el-input id="secondVal" v-model="secondVal" disabled />
+      </el-form-item>
+      <el-form-item label="毫秒">
+        <el-input id="millisecondVal" v-model="millisecondVal" disabled />
       </el-form-item>
     </el-form>
   </div>
@@ -36,8 +64,17 @@ export default {
   data() {
     return {
       labelPosition: 'top',
-      input: '',
-      output: ''
+      date: '',
+      timestamp: '',
+      dateType: '',
+      datetime: '',
+      dateVal: '',
+      timestampVal: '',
+      dayVal: '',
+      hourVal: '',
+      minuteVal: '',
+      secondVal: '',
+      millisecondVal: ''
     }
   },
   methods: {
